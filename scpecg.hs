cc8c37cca3c430a578e3f08fb8c5159105a41d73
Peter Wyrd <thewyrdguy@gmail.com>
2019-07-18 12:09:00 +0200

    Refactor Either

--- scpecg.hs
+++ scpecg.hs
@@ -17,30 +17,36 @@ skip 0 = return ()
 skip n = getWord8 >> skip (n - 1)
 
 parseSCPsection :: SCPRec -> Integer -> Word16
-                -> Get ((Either String SCPRec), Integer, Word16)
+                -> Get (Either String (SCPRec, Integer, Word16))
 parseSCPsection scprec remains crc
-  | remains == 0 = return $! (Right scprec, 0, 17652)
-  | remains < 8  = return $! (Left ("short data " ++ (show remains)), 0, crc)
+  | remains == 0 = return $! Right (scprec, 0, 17652)
+  | remains < 8  = return $! Left ("short data " ++ (show remains))
   | otherwise = do
     seccrc <- getWord16le
     sec_id <- getWord16le
     seclen <- getWord32le
     skip ((fromIntegral seclen) - 8)
-    let newrec = SCPRec {sec11 = Just ((fromIntegral sec_id):(fromIntegral seclen):(fromMaybe [] (sec11 scprec)))}
+    let newrec = SCPRec {sec11 =
+      Just ((fromIntegral sec_id)
+           :(fromIntegral seclen)
+           :(fromMaybe [] (sec11 scprec)))
+      }
     parseSCPsection newrec (remains - (fromIntegral seclen)) crc
-
 
 parseSCPfile :: Integer -> Get (Either String SCPRec)
 parseSCPfile realsize = do
   expectedcrc <- getWord16le
   expectedsize <- getWord32le
   if realsize == (fromIntegral expectedsize) then do
-      (scprec, _, realcrc) <- parseSCPsection emptySCPRec (realsize - 6) 0
-      if realcrc == expectedcrc then
-          return scprec
-        else
-          return $ Left $ "expected csum:" ++ (show expectedcrc)
-                               ++ " real:" ++ (show realcrc)
+      parseout <- parseSCPsection emptySCPRec (realsize - 6) 0
+      case parseout of
+        Right (scprec, _, realcrc) ->
+          if realcrc == expectedcrc then
+              return $ Right scprec
+            else
+              return $ Left $ "expected csum:" ++ (show expectedcrc)
+                                   ++ " real:" ++ (show realcrc)
+        Left err -> return $ Left err
     else
       return $ Left $ "csum:" ++ (show expectedcrc)
                   ++ " expected:" ++ (show expectedsize)
