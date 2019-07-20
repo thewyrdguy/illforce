0cb90aab80ab9a6c6135b0e919de9837c771d7fb
Peter Wyrd <thewyrdguy@gmail.com>
2019-07-20 15:52:08 +0200

    separate frontend from file processing

---
+++ SCPECG.hs
@@ -0,0 +1,21 @@
+module SCPECG (parseSCPFiles, SCPRec) where
+
+import System.IO (IOMode(ReadMode), openBinaryFile, hSetBinaryMode
+                 ,hIsSeekable, hFileSize, stdin)
+import Data.ByteString.Lazy (hGetContents)
+import SCPECG.Core
+
+parseSCPFiles :: [String] -> IO [(Either String SCPRec)]
+parseSCPFiles fnames = do
+  hdls <- case fnames of
+    [] -> hSetBinaryMode stdin True >> return [stdin]
+    otherwise -> mapM (\fn -> openBinaryFile fn ReadMode) fnames
+  conts <- mapM size_n_contents hdls
+  return $ map (uncurry parseSCP) conts
+  where
+    size_n_contents fh = do
+      asksize <- hIsSeekable fh
+      maybesize <- if asksize then hFileSize fh >>= return . Just
+                              else return Nothing
+      contents <- hGetContents fh
+      return (maybesize, contents)
--- scpecg.hs
+++ scpecg.hs
@@ -1,21 +1,6 @@
 import System.Environment (getArgs)
-import System.IO (IOMode(ReadMode), openBinaryFile, hSetBinaryMode
-                 ,hIsSeekable, hFileSize, stdin)
-import Data.ByteString.Lazy (hGetContents)
-import SCPECG.Core
+import SCPECG
 
 main = do
   args <- getArgs
-  hdls <- case args of
-    [] -> hSetBinaryMode stdin True >> return [stdin]
-    otherwise -> mapM (\fn -> openBinaryFile fn ReadMode) args
-  conts <- mapM size_n_contents hdls
-  let scprecs = map (\(size, cont) -> parseSCP size cont) conts
-  print scprecs
-  where
-    size_n_contents fh = do
-      asksize <- hIsSeekable fh
-      maybesize <- if asksize then hFileSize fh >>= return . Just
-                              else return Nothing
-      contents <- hGetContents fh
-      return (maybesize, contents)
+  parseSCPFiles args >>= print
