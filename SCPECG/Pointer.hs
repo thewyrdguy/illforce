9d572b36abc09a460bf0b57e7fabf39df4d69cad
Peter Wyrd <thewyrdguy@gmail.com>
2019-07-27 23:09:28 +0200

    implement section0 (Pointer) parsing

--- SCPECG/Pointer.hs
+++ SCPECG/Pointer.hs
@@ -2,18 +2,34 @@ module SCPECG.Pointer (SCPPointer) where
 
 import Data.ByteString.Char8 (pack)
 import Data.ByteString (ByteString)
-import Data.Binary.Get ( Get, runGet, isolate, skip
+import Data.Binary.Get ( Get, runGet, isEmpty, isolate, skip
                        , getWord8, getWord16le, getWord32le
                        , getByteString)
 import Data.Word (Word8, Word16, Word32)
 
 import SCPECG.Types
 
-data SCPPointer = SCPPointer { scpPointerMagic :: ByteString
+data SCPPointer = SCPPointer { scpPointerIdx :: [(Word16, Word32, Word32)]
                              } deriving Show
 
 instance SCPSection SCPPointer where
 --parseSection :: Integer -> Word16 -> Get (Either String SCPPointer)
   parseSection size _ = do
-    dat <- getByteString (fromIntegral size)
-    return $ Right $ SCPPointer (pack "pOiNteR")
+    skip 8
+    pfx <- getByteString 8
+    idx <- getIdx
+    return $ if pfx == (pack "\r\rSCPECG")
+      then Right $ SCPPointer idx
+      else Left $ "Unexpected magic string: " ++ (show pfx)
+    where
+      getIdx :: Get [(Word16, Word32, Word32)]
+      getIdx = do
+        empty <- isEmpty
+        if empty
+          then return []
+          else do
+            id <- getWord16le
+            len <- getWord32le
+            off <- getWord32le
+            idx' <- getIdx
+            return $ (id, off, len):idx'
