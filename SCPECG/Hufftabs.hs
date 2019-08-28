21be96c69c59a87221d99133bdf2f8d3be3f34ec
Peter Wyrd <thewyrdguy@gmail.com>
2019-08-29 00:17:10 +0200

    Basic section 2 "Huffmann tables" parser

--- SCPECG/Hufftabs.hs
+++ SCPECG/Hufftabs.hs
@@ -1,5 +1,9 @@
-module SCPECG.Hufftabs (SCPHufftabs) where
+module SCPECG.Hufftabs ( SCPHufftabs(..)
+                       , SCPHuffTable(..)
+                       , SCPHuffCodeStruct(..)
+                       ) where
 
+import Control.Monad (replicateM)
 import Data.ByteString.Lazy (ByteString)
 import Data.Binary.Get ( Get, runGet, isEmpty, isolate, skip
                        , getWord8, getWord16le, getWord32le
@@ -8,8 +12,19 @@ import Data.Word (Word8, Word16, Word32)
 
 import SCPECG.Types
 
-data SCPHufftabs = SCPHufftabs { scpHuffNTables :: Word16
-                               , scpHuffData :: [Word8]
+data SCPHuffCodeStruct = SCPHuffCodeStruct { scpHuffCodeBitsInPrefix :: Word8
+                                           , scpHuffCodeBitsInCode   :: Word8
+                                           , scpHuffCodeModeSwitch   :: Word8
+                                           , scpHuffCodeBaseValue    :: Word16
+                                           , scpHuffCodeBaseCode     :: Word32
+                                           } deriving Show
+
+data SCPHuffTable = SCPHuffTable { scpHuffCodeStructsNum :: Word16
+                                 , scpHuffCodeStructs :: [SCPHuffCodeStruct]
+                                 } deriving Show
+
+data SCPHufftabs = SCPHufftabs { scpHuffTablesNum :: Word16
+                               , scpHuffTables  :: [SCPHuffTable]
                                } deriving Show
 
 instance SCPSection SCPHufftabs where
@@ -17,24 +32,26 @@ instance SCPSection SCPHufftabs where
   parseSection size _ = do
     skip 16
     ntabs <- getWord16le
-    -- if ntabs == 19999 there will be no data, use default tables.
-    -- if ntabs > 0, the first two bytes in the table is
-    -- "Number of code structures in table"
-    -- But we do not know what to do with them.
-    -- Experimentally, I saw 1 table with 1 "code structure" and there
-    -- where 10 bytes there: [0,16,1,0,0,0,8,0,0,0]
-    dat <- getDat
-    return $ Right $ SCPHufftabs ntabs dat
+    if ntabs == 19999
+      then return $ Right $ SCPHufftabs ntabs []
+      else do
+        tables <- replicateM (fromIntegral ntabs) getTable
+        return $ Right $ SCPHufftabs ntabs tables
     where
-      getDat :: Get [Word8]
-      getDat = do
-        empty <- isEmpty
-        if empty
-          then return []
-          else do
-            byte <- getWord8
-            dat' <- getDat
-            return $ byte:dat'
+      getTable :: Get SCPHuffTable
+      getTable = do
+        numstructs  <- getWord16le
+        codestructs <- replicateM (fromIntegral numstructs) getCode
+        return $ SCPHuffTable numstructs codestructs
+      getCode :: Get SCPHuffCodeStruct
+      getCode = do
+        bitsInPfx  <- getWord8
+        bitsInCode <- getWord8
+        modeSwitch <- getWord8
+        baseValue  <- getWord16le
+        baseCode   <- getWord32le
+        return $ SCPHuffCodeStruct bitsInPfx bitsInCode modeSwitch
+                 baseValue baseCode
 
 instance Semigroup SCPHufftabs where
   x <> y = x
