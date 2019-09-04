24cb2d1bf5cd5fbbea9264af373ee22f55371371
Peter Wyrd <thewyrdguy@gmail.com>
2019-09-04 21:58:54 +0200

    make record animating prototype with gloss

---
+++ animate.hs
@@ -0,0 +1,39 @@
+import Data.IORef
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Animate
+main = do
+  datap <- newIORef $ replicate 4000
+                         (0.0 :: Float, 0.0 :: Float, 0 :: Int, 0 :: Int)
+  animateFixedIO (InWindow "Signal" (1280, 720) (500, 500)) white
+    (step datap) ctrl
+
+step :: IORef [(Float, Float, Int, Int)] -> Float -> IO Picture
+step datap tm = do
+  indata <- readIORef datap
+  newdata <- advance indata tm
+  writeIORef datap newdata
+  return $ picture newdata
+
+advance :: [(Float, Float, Int, Int)]
+        -> Float
+        -> IO [(Float, Float, Int, Int)]
+advance indata atm
+  | atm < (let (x, _, _, _) = (head indata) in x) = return indata
+  | otherwise = do
+    new <- fmap ((\[s0, s1, s2, s3] ->
+                   ( read s0 :: Float
+                   , read s1 :: Float
+                   , read s2 :: Int
+                   , read s3 :: Int)) . words) $ getLine
+    advance (take 4000 (new:indata)) atm
+
+picture indata =
+  translate (600) (-350) $ line (map fit (zip [0..] indata))
+  where
+    fit :: (Int, (Float, Float, Int, Int)) -> (Float, Float)
+    fit (num, (ts, mV, qrs, ano)) =
+      ((fromIntegral num) * (-10280.0) / 4000.0, mV * 720.0 / 3.0)
+      
+
+ctrl :: Controller -> IO ()
+ctrl req = putStrLn "ctrl callback"
