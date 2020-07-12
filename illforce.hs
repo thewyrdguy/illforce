54883db0efcd682447ab82acc98c7d49281d5bdf
Peter Wyrd <thewyrdguy@gmail.com>
2020-07-12 23:51:11 +0200

    Make text output working

--- illforce.hs
+++ illforce.hs
@@ -2,15 +2,25 @@ module Main where
 
 import System.Exit (exitWith, ExitCode(..))
 import System.Environment (getArgs, getProgName)
+import System.IO (writeFile)
+import System.FilePath (FilePath, (</>))
 import System.Console.GetOpt (getOpt, ArgOrder(Permute), usageInfo,
                               OptDescr(Option), ArgDescr(..))
+import Text.Printf (printf)
+import Data.List (find)
+import Data.Word (Word16)
+import Data.Bits ((.&.), testBit)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (ExceptT(ExceptT), runExceptT)
+import Control.Exception (try)
 import SCPECG
 import SCPECG.Signal (SCPSignal(..))
 import ILLFORCE
 
-data Operation = OpUnspec | OpList deriving (Eq, Show)
+data Operation = OpUnspec
+               | OpList
+               | OpText
+               deriving (Eq, Show)
 
 data Options = Options { optVerbose :: Bool
                        , optOp      :: Operation
@@ -32,6 +42,9 @@ options =
   , Option ['L'] ["list"]
       (NoArg (\opts -> opts { optOp = OpList }))
       "List Records"
+  , Option ['T'] ["text"]
+      (NoArg (\opts -> opts { optOp = OpText }))
+      "Translate records to text format"
   , Option ['O'] ["out"]
       (ReqArg (\d opts -> opts { optOut = d }) "DIR")
       "Output directory"
@@ -60,9 +73,38 @@ main =
     drec <- ExceptT $ listIllForceDir (optPath opts)
     case optOp opts of
       OpList -> listrecs opts drec
+      OpText -> outtexts opts drec
   ) >>= either (\err -> print err >> exitWith (ExitFailure 1))
                (\_   -> return ())
 
 listrecs opts dirrec = do
   let heads = map (\(n, xs) -> (n, head xs)) dirrec
   lift $ mapM (putStrLn . show) heads
+
+outtexts opts dirrec = mapM (outtext opts) dirrec
+  where
+  outtext opts (n, paths) = do
+    let outname = (optOut opts) </> (show n) ++ ".txt"
+    record <- ExceptT $ try $ parseSCPFiles paths
+    signal <- ExceptT $ return $ findsignal record
+    ExceptT $ try $ writeFile outname (unlines (formattext signal))
+  findsignal []                  = Left $ userError "Section 6 is absent"
+
+  findsignal ((Right (S6 s)):xs) = Right s
+  findsignal ((Right _):xs)      = findsignal xs
+  findsignal ((Left er):xs)      = Left $ userError er
+
+  formattext signal = map formatentry (normalize signal)
+  normalize signal = map (normentry (scpSignaluSper1 signal)
+                                    (scpSignalnVper1 signal))
+                            (zip [0 ..] (scpSignalData signal))
+  normentry :: Word16 -> Word16 -> (Int, Word16) -> (Float, Float, Int, Int)
+  normentry period multiplier (sample, value) =
+    ( (fromIntegral period) * (fromIntegral sample) * 0.000001
+    , (fromIntegral multiplier) * (fromIntegral (value .&. 0x0fff)) * 0.000001
+    , if testBit value 15 then 1 else 0
+    , if testBit value 14 then 1 else 0
+    )
+  formatentry :: (Float, Float, Int, Int) -> String
+  formatentry (sec, volt, isqrs, isanomaly) =
+    printf "%9.3f %07.5f %1d %1d" sec volt isqrs isanomaly
